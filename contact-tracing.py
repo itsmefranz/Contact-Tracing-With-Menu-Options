3915741a850268ae5a72551da07397b05d7ddc47
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-11-12 22:11:41 +0800

    menu option numbers 1 and 3 works properly unfortunately still havent found a solution for menu option number 3.

--- contact-tracing.py
+++ contact-tracing.py
@@ -12,6 +12,46 @@
 #		- Option 2: Search, ask full name then display the record
 #		- Option 3: Ask the user if want to exit or retry.
 
+def menu_program():
+        menu= input("What would you like to do? [1-3] ")
+
+        if menu=="1":
+            personal_info={}
+            name = input("Enter user's full name: ")
+            age= input("Enter user's age: ")
+            address= input("Enter user's home address: ")
+            phone_num= int(input("Enter user's phone number: "))
+
+            info ={
+                "Full Name" : name,
+                "Age" : age,
+                "Home Address" : address,
+                "Phone Number" : phone_num
+                }
+                
+            personal_info[name] = info
+
+
+            print(personal_info)
+            print("Saved!")
+
+        if menu=="2":
+            name= input("What is the full name of the person? " )
+
+            print(personal_info[name])
+
+        if menu=="3":
+                retry_exit = input("Do you wish to exit? (ㅅ´ ˘ `) Y/N: ")
+                if retry_exit == "n":
+                    print("\n")
+                    menu_program()
+                elif retry_exit == "y":
+                    print("\n.・。.・゜✭・.・✫・゜・。.・。.・゜✭・.・✫・゜・。.")
+                    print("See ya next time! Bye for now ◝(ᵔᵕᵔ)◜\n")
+                else:
+                    print("\nEnter Y if you want to exit, or N to retry the program.")
+                    exit()
+
 print("\n┏━· • —– ٠ ✤ ٠ —– • · • —– ٠ ✤ ٠ —– • · • —– ٠ ✤ ٠ —– • ·━┓")
 print ("""
     Menu
@@ -21,37 +61,4 @@ print ("""
     """)
 print("┗━· • —– ٠ ✤ ٠ —– • · • —– ٠ ✤ ٠ —– • · • —– ٠ ✤ ٠ —– • ·━┛\n") 
 
-personal_info = {}
-while True:
-    def menu_program():
-        menu=int(input("What would you like to do? [1-3] "))
-
-    if menu=="1":
-        personal_info={}
-        name = input("Enter user's full name: ")
-        age= input("Enter user's age: ")
-        address= input("Enter user's home address: ")
-        phone_num = input("Enter user's phone number: ")
-
-        personal_info[name] = age, address, phone_num
-
-        print(personal_info)
-        print("Saved!")
-
-    
-    
-    if menu=="3":
-        while True:
-            retry_exit = input("Do you wish to exit? (ㅅ´ ˘ `) Y/N: ")
-            if retry_exit == "n":
-                print("\n")
-                menu_program()
-            elif retry_exit == "y":
-                print("\n.・。.・゜✭・.・✫・゜・。.・。.・゜✭・.・✫・゜・。.")
-                print("See ya next time! Bye for now ◝(ᵔᵕᵔ)◜\n")
-            else:
-                print("\nEnter Y if you want to exit, or N to retry the program.")
-                exit()
-
-    menu_program()
-        
+menu_program()
